8cfb49406ad2f2dd4b9af1703ce2b71cd64f71cc
KALEB DIAS <kalebdias.oficial@gmail.com>
2018-07-16 09:45:29 -0300

    Criação do Modelo de Dados Produto

---
+++ CadastroProdutosMVC/CadastroProdutosMVC/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastroProdutosMVC.Models
+{
+    public class Produto
+    {
+        public string IDproduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string descricaoProduto { get; set; }
+
+        public double valorUniProduto { get; set; }
+        
+    }
+}
