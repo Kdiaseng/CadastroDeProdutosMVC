bade08ca94949d8bc33e159252424c7a9cf0f324
KALEB DIAS <kalebdias.oficial@gmail.com>
2018-07-16 10:12:26 -0300

    Adicionado a string de conexão

--- CadastroProdutosMVC/CadastroProdutosMVC/Models/Produto.cs
+++ CadastroProdutosMVC/CadastroProdutosMVC/Models/Produto.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CadastroProdutosMVC.Models
 {
     public class Produto
     {
+        [Key]
         public string IDproduto { get; set; }
+
+        [Required]                              //Será obrigatorio o cadastro do nome do produto
         public string NomeProduto { get; set; }
+
         public string descricaoProduto { get; set; }
 
+        [Required]
         public double valorUniProduto { get; set; }
         
     }
