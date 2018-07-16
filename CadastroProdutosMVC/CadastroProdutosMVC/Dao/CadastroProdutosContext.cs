8668b3d6331182b11f913a6ebd39bc2eed56b956
KALEB DIAS <kalebdias.oficial@gmail.com>
2018-07-16 09:38:59 -0300

    Criação da Classe de Context Framwork com Intity

---
+++ CadastroProdutosMVC/CadastroProdutosMVC/Dao/CadastroProdutosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CadastroProdutosMVC.Dao
+{
+    public class CadastroProdutosContext: DbContext
+    {
+        public CadastroProdutosContext():
+            base("produtosContext")
+        {
+
+        }
+
+        public DbSet<Produto> produto { get; set; }
+    }
+}
